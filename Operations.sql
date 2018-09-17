463479fae1a1762bfebfffc28e700e56a5877e72
Ajay Raj <najayraj.16.cse@anits.edu.in>
2018-09-17 18:54:13 +0530

    Reformatted sql file
    I want you guys to follow this format STRICTLY. Understand that this file is read by our program and every query is executed individually.
    
    We cannot drop any triggers. Its okay even if we do not use procedures we will use fucntions which are even better.
    
    No blank lines anywhere
    -- comments shoudl start and end like this --
    changing the delimiter is a seperate query not a combied one so keep it seperate.
    
    I dont care what you write in the comments, without comments the parsing will fail, so you may fill it up with shit, but only one line

--- Operations.sql
+++ Operations.sql
@@ -1,73 +1,49 @@
--- for dropping the trigger we use the procedures so that it will also be covered in our project -- 
---for calling any procedure we simply write  call procedure_name() -- 
---Before writing trigger we have to change the  delimiter from ; to $ --
-
---creating triggers and procedures on Student table only ---
-
-create procedure drop_before_student_update()
-drop trigger before_student_update;
-
-create procedure drop_after_student_update()
-drop trigger after_student_update;
-
-create procedure drop_after_student_delete()
-drop trigger after_student_delete;
-
-create procedure drop_after_student_insertion()
-drop trigger after_student_insertion;
-
-create procedure drop_before_student_insertion()
-drop trigger before_student_insertion;
-
-""" trigger on before update """
-
+-- Changing Delimiter --
 DELIMITER $
+-- Creating trigger for audit insert
 create trigger before_student_update before update on Student 
 for each row 
 begin 
 	insert into Student_audit
     	set reg_id = old.reg_id,
         name = old.name,
-	marks = old.marks,
-	action = 'before update',
+		marks = old.marks,
+		action = 'before update',							
         change_of_time = timestamp(now()); 
 end$
+-- Resetting Delimiter --
 DELIMITER ;
-
-""" trigger on after update """
-
+-- Changing Delimiter --
 DELIMITER $
+-- Creting trigger for audit insert --
 create trigger after_student_update after update on Student 
 for each row 
 begin 
 	insert into Student_audit
     	set reg_id = old.reg_id,
         name = new.name,
-	marks = new.marks,
-	action = 'after update',
+		marks = new.marks,
+		action = 'after update',
         change_of_time = timestamp(now()); 
 end$
+-- Resetting Delimiter --
 DELIMITER ;
-
---as there is no need of trigger for before delete .. we write it only for after deletion --
-""" trigger on after delete """
-
-DELIMITER $
+-- Creating triiger for audit insert --
 create trigger after_student_delete after delete on Student 
 for each row 
 begin 
 	insert into Student_audit
     	set reg_id = old.reg_id,
         name = old.name,
-	marks = old.marks,
-	action = 'delete',
+		marks = old.marks,
+		action = 'delete',
         change_of_time = timestamp(now()); 
 end$
+-- Resetting delimiter --
 DELIMITER ;
- 
-""" trigger on after insertion """
-
+-- Changing Delimiter --
 DELIMITER $
+-- Creating trigger --
 create trigger after_student_insertion after insert on Student 
 for each row 
 begin 
@@ -78,22 +54,16 @@ begin
 	action = 'insertion',
 	change_of_time = timestamp(now());
 end$
+-- Resetting delimiter --
 DELIMITER ; 
-
---as we are giving the constraints as not null for all attributes if we make condition on attribute if it not satisfied we make it as null then it immediately raises an error --
-"""trigger on before insertion """
-
+-- changind delimiter --
 DELIMITER $
+-- creting trigger
 create trigger before_student_insertion before insert on Student 
 for each row 
 begin 
 	if length(reg_id) < 12 then set reg_id = null
-	if marks > 10 then set marks = null 	
+	if marks > 15 then set marks = null 	
 end$
-DELIMITER ; 
-
-	
-  
---creating triggers and procedures on Script table only ---
-
-	
+-- Resetting delimiter
+DELIMITER ;
