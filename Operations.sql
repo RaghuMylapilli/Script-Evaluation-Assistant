612444cdbfd600657338916783e58bf8716e4662
mylapilli raghu <raghuanits16@gmail.com>
2018-09-18 21:23:01 +0530

    Update Operations.sql

--- Operations.sql
+++ Operations.sql
@@ -70,8 +70,13 @@ end;
 --Creating trigger before insert on grade table tested--
 create trigger before_grade_insert before insert on Grade
 for each row
-begin
-	
+begin 
+	if length(new.reg_id) < 12 then signal sqlstate '45231';
+	end if;
+	if new.grade > 10 then signal sqlstate '34257';
+	end if;
+	if new.date_of_grading != curdate() then signal sqlstate '56742';
+	end if;
 end;
 --Creating trigger after insert on grade table tested--
 create trigger after_grade_insert after insert on Grade
